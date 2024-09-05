57458b1e96b03eacea540af5721785849a82d8d1
Razhok Aliaksandr <aliaksandr.razhok.std@gmail.com>
2024-09-05 14:00:11 +0400

    Write unit tests for GetApiKey func, change ci workflow

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,9 +14,10 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
+
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
+		return "", errors.New("malformed authorization header!")
 	}
 
 	return splitAuth[1], nil
